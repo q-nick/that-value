3c9146b6b718358f8da79f9c623d3a38231b3189
Paweł Stefański <pawel.stefanski@rkantor.com>
2016-02-17 17:30:47 +0100

    validators draft

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function(value) {
+    return new thatValue(value);
+};
+
+function thatValue(value) {
+    //validators
+    var validators = {};
+    validators.number = require('./lib/validator/number/number.js').bind(this);
+    validators.greaterThan = require('./lib/validator/greater/greater.js').bind(this);
+
+    var validations = [];
+
+    this.pushValidation = function(validation) {
+        validations.push(validation);
+    };
+
+    this.addValidator = function(key, validation) {
+        validators[key] = validation.bind(this);
+    };
+
+    this.valid = function() {
+        for (var i = 0; i < validations.length; i++) {
+            if (!validations[i](value)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    this.is = validators;
+    this.contains = validators;
+    this.and = validators;
+    this.or = this;
+}
